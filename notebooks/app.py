c978b8dfeed0be36e0dd3613982a75813aab97f4
EmmanuelV9 <emmanuel353.ev@gmail.com>
2025-04-17 19:16:12 -0600

    Cuarto intento de subir y sincroni

--- notebooks/app.py
+++ notebooks/app.py
@@ -21,3 +21,4 @@ build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
+    
