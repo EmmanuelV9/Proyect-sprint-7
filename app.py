19cb87d21e1faec0878211ca1ef6c33d543d02e2
EmmanuelV9 <emmanuel353.ev@gmail.com>
2025-04-17 20:26:22 -0600

    Cambios app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+hist_button = st.button('Construir histograma')
+st.header('Histograma y grafico de conjunto de datos venta de coches')
         
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
@@ -17,8 +18,9 @@ if hist_button: # al hacer clic en el botón
 
 
 # crear una casilla de verificación
-build_histogram = st.checkbox('Construir un histograma')
+build_histogram = st.button('Construir un grafico de dispersion')
 
 if build_histogram: # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
-    
+    st.write('Construir un grafico de dispersion para la columna odómetro')
+    fig = px.scatter(car_data,x="odometer",y="price")
+    st.plotly_chart(fig, use_container_width=True)
